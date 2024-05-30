7547d929bec7d46c684524968bd836476460183f
Jane49-cloud <janendirangu49@gmail.com>
2024-05-31 01:19:44 +0300

    debug

--- src/data/index.jsx
+++ src/data/index.jsx
@@ -12,36 +12,50 @@ export const links = [
 
 export const courses = [
   {
-    name: "Accounting Basics",
+    title: "Accounting Basics",
     lessons: 7,
-    completion: 25,
-    image:
+    progress: 25,
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    thumbnail:
       "https://img.freepik.com/free-photo/businesspeople-working-finance-accounting-analyze-financi_74952-1411.jpg?t=st=1717075538~exp=1717079138~hmac=2d467bc3d99eaa5fcdf8b157cd794c3cc7e9241fa99fa53f60703c5ce1e1d701&w=1380",
   },
   {
-    name: "Marketing 101",
+    title: "Marketing 101",
     lessons: 10,
-    completion: 50,
-    image:
+    progress: 50,
+    progress: 25,
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    thumbnail:
       "https://img.freepik.com/free-photo/marketing-advertising-commercial-strategy-concept_53876-13733.jpg?t=st=1717075711~exp=1717079311~hmac=92591808bc41bc4ed95157c57003c6d34ccfec7d625f450e1f1c84205a2c2f87&w=1380",
   },
   {
-    name: "Introduction to Python",
+    title: "Introduction to Python",
     lessons: 5,
-    completion: 75,
-    image:
+    progress: 75,
+    progress: 25,
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    thumbnail:
       "https://img.freepik.com/free-photo/scripting-computer-language-code-programming-developer-technology-concept_53876-125414.jpg?w=826&t=st=1717075862~exp=1717076462~hmac=c8c76a7914eb9166c40f4f34316f622de3f5f924724b8f13a7308747a3c31fc2",
   },
   {
-    name: "Project Management",
+    title: "Project Management",
     lessons: 8,
-    completion: 40,
-    image: image,
+    progress: 40,
+    thumbnail: Image,
+    progress: 25,
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
   },
   {
-    name: "Accounting Basics",
+    title: "Accounting Basics",
     lessons: 7,
     completion: 25,
+    progress: 25,
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
     image:
       "https://img.freepik.com/free-photo/businesspeople-working-finance-accounting-analyze-financi_74952-1411.jpg?t=st=1717075538~exp=1717079138~hmac=2d467bc3d99eaa5fcdf8b157cd794c3cc7e9241fa99fa53f60703c5ce1e1d701&w=1380",
   },
